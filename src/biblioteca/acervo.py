e6ec2bd38d000634ff8a20ea4d57391510ff0ef8
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-02-16 20:32:34 -0300

    Update acervo.py

--- src/biblioteca/acervo.py
+++ src/biblioteca/acervo.py
@@ -2,7 +2,7 @@ class Acervo:
     def __init__(self):
         self.__acervo = {}
     
-    def getacervo(self):
+    def getAcervo(self):
         return self.__acervo
     def adicionar_livro(self,livro,quantidade):
         idlivro = livro.get_id()
@@ -14,7 +14,29 @@ class Acervo:
                 "Quantidade": quantidade
             }    
     
-
+    def remover_livro_nome(self,nome,quantidade):
+            for n in self.__acervo.values():
+                livro = n['livro']
+                if livro.titulo.lower() == nome.lower():
+                    if  n['Quantidade'] < quantidade:
+                        raise ValueError("Quantidade indisponivel")
+                    else:
+                        n['Quantidade'] -= quantidade
+                        return True
+            return False
+    
+    def remover_livro_id(self,id,quantidade = None):
+        if id in self.__acervo:
+            if quantidade is None:
+                del self.__acervo[id]
+            else:
+                if self.__acervo[id]['Quantidade'] < quantidade:
+                    raise ValueError("Quantidade indisponivel")
+                else:
+                    self.__acervo[id]['Quantidade'] -= quantidade
+        else:
+            raise ValueError("Livro não encontrado")
+      
 
     def consultar_acervo(self):
         acervo = self.__acervo
