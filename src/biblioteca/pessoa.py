98b55bf7b2ff50803b93aed2e1c38cad4d81920d
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-02-16 14:57:10 -0300

    Create pessoa.py

---
+++ src/biblioteca/pessoa.py
@@ -0,0 +1,27 @@
+from abc import ABC , abstractmethod
+from src.validacoes.validacoes import validar_formato_cpf
+from datetime import datetime
+class Pessoa(ABC):
+    def __init__(self,nome:str,cpf:str,data_nascimento:datetime):
+        self.nome = nome
+        self.__cpf = validar_formato_cpf(cpf)
+        self.__data_nascimento = data_nascimento
+        
+    @abstractmethod
+    def __str__(self):
+        return f"Nome: {self.nome} | Cpf: {self.get_cpf()} | Data de Nascimento: {self.get_data_nascimento()}"
+    def get_cpf(self):
+        '''Retorna Cpf da Pessoa'''
+        return self.__cpf
+    def get_data_nascimento(self):
+        '''Retorna Data de Nascimento da Pessoa'''
+        return self.__data_nascimento
+    def set_cpf(self,cpf_novo):
+        if not validar_formato_cpf(cpf_novo):
+            raise ValueError("CPF Inválido")
+        else:
+            self.__cpf = cpf_novo
+            return True
+    
+        
+    
