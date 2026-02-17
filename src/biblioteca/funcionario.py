5465551364216e31642b41dc72744308992a76d0
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-02-17 15:07:42 -0300

    Update funcionario.py

--- src/biblioteca/funcionario.py
+++ src/biblioteca/funcionario.py
@@ -1,14 +1,16 @@
 from src.biblioteca.pessoa import Pessoa
 class Funcionario(Pessoa):
-    def __init__(self, nome, cpf, data_nascimento,salario:float,cargo:str):
+    def __init__(self, nome, cpf, data_nascimento,salario:float,cargo:str,biblioteca):
         super().__init__(nome, cpf, data_nascimento)
         self.__salario = salario
         self.__cargo = cargo
+        self.__biblioteca = biblioteca
 
     def get_salario(self):
         return self.__salario
     def get_cargo(self):
         return self.__cargo
-
+    def get_biblioteca(self):
+        return self.__biblioteca
     def __str__(self):
         return f"Nome: {self.nome} | Cargo: {self.get_cargo()} | Salário: {self.get_salario()}"
