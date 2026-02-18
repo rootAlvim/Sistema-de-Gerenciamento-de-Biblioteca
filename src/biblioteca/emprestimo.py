198c62d3059bda6278011f9b8c4977ef5b9c2197
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-02-18 10:59:41 -0300

    Update emprestimo.py

--- src/biblioteca/emprestimo.py
+++ src/biblioteca/emprestimo.py
@@ -1,4 +1,4 @@
-
+from src.biblioteca.livro import Livro
 class Emprestimo:
     def __init__(self,id,usuario,data_emprestimo):
         self.__id = id
@@ -15,4 +15,44 @@ class Emprestimo:
     def getDataEmprestimo(self):
         return self.__data_emprestimo
     def __repr__(self):
-        return f'Id: {self.getId()} | Usuario: {self.getUsuario().nome} | Data: {self.getDataEmprestimo()}'
+        return f'Id do Emprestimo: {self.getId()} | Usuario: {self.getUsuario().nome} | Data: {self.getDataEmprestimo()}'
+    
+    def Adicionar_livro(self,livro,qntd):
+        if int(qntd) <= 0:
+            raise ValueError('Quantidade deve ser maior que 0')
+        
+        for item in self.__livros:   
+            if livro.get_id() == item.id:
+                item.quantidade += qntd
+                return True
+
+        self.__livros.append(
+            Item(
+                livro.titulo,
+                livro.autor,
+                livro.editora,
+                livro.edicao,
+                livro.ano_publicacao,
+                livro.genero,
+                livro.get_preco(),
+                livro.get_id(),
+                int(qntd)
+            )
+        )
+
+
+
+
+class Item:
+    def __init__(self,titulo,autor,editora,edicao,ano_publicacao,genero,preco,id,quantidade):
+        self.titulo = titulo
+        self.autor = autor
+        self.editora = editora
+        self.edicao = edicao
+        self.ano_publicacao = ano_publicacao
+        self.genero = genero
+        self.preco = preco
+        self.id = id
+        self.quantidade = quantidade
+    def __repr__(self):
+        return f"Id={self.id}, Nome={self.titulo}, Preço={self.preco}, Quantidade: {self.quantidade}"
