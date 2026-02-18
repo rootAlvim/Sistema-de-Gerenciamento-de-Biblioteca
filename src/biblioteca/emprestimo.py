90ab6e0474ab9b5a9a1dc059576bf068f020bc09
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-02-18 15:38:45 -0300

    Update emprestimo.py

--- src/biblioteca/emprestimo.py
+++ src/biblioteca/emprestimo.py
@@ -5,6 +5,7 @@ class Emprestimo:
         self.__usuario = usuario
         self.__data_emprestimo = data_emprestimo
         self.__livros = []
+        self.__data_entrega = 0
 
     def getId(self):
         return self.__id
@@ -14,10 +15,15 @@ class Emprestimo:
         return self.__livros
     def getDataEmprestimo(self):
         return self.__data_emprestimo
+    def getDataEntrega(self):
+        return self.__data_entrega
+    
     def __repr__(self):
         return f'Id do Emprestimo: {self.getId()} | Usuario: {self.getUsuario().nome} | Data: {self.getDataEmprestimo()}'
     
     def Adicionar_livro(self,livro,qntd):
+        if self.__data_entrega:
+            raise PermissionError("Emprestimo ja finalizado")
         if int(qntd) <= 0:
             raise ValueError('Quantidade deve ser maior que 0')
         
@@ -40,8 +46,24 @@ class Emprestimo:
             )
         )
 
+    def Remover_livro(self,livro,qntd):
+        if self.__data_entrega:
+            raise PermissionError("Emprestimo já finalizado")
+        if int(qntd) <= 0:
+            raise ValueError("Quantidade deve ser maior que 0")
 
-
+        for item in self.__livros:
+            if livro.get_id() == item.id:
+                if item.quantidade < qntd:
+                    raise ValueError("Quantidade excede valor disponível para remoção")
+                item.quantidade -= qntd
+                return True
+        raise ValueError("Livro não está adicionado no emprestimo")
+    
+    def Finalizar_emprestimo(self):
+        if self.__data_entrega:
+            raise PermissionError("Emprestimo já finalizado")
+        
 
 class Item:
     def __init__(self,titulo,autor,editora,edicao,ano_publicacao,genero,preco,id,quantidade):
