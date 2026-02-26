e81d60948e994751663c2b865f19bfc39b94c609
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-02-26 11:14:05 -0300

    Update test_usuario.py

--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -1,9 +1,10 @@
 from src.biblioteca.usuario import Usuario
 U1 = Usuario(
-    "Vanessa",
-    "12345678909",
-    "12-1-1001",
+    "Vanessa Camargo Silva Pinto",
+    "34562345678",
+    "12-11-1999",
     1,
     "Rua joão preto, numero 13, bairro cascalho"
 )
 print(U1)
+print(f'{U1.get_compras()}\n{U1.get_cpf()}\n{U1.get_data_nascimento()}\n{U1.get_emprestimos()}\n{U1.get_endereco()}\n{U1.get_pendencias()}')
