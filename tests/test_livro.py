cc98c646824556a8ac491ac14b82d335302081b2
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-02-26 11:18:23 -0300

    Create test_livro.py

---
+++ tests/test_livro.py
@@ -0,0 +1,12 @@
+from src.biblioteca.livro import Livro
+livro = Livro(
+    'João e Maria',
+    'Chico Buarque',
+    'Moderna',
+    12,
+    '12-02-1998',
+    'Fantasia',
+    2.90,
+    2
+)
+print(livro)
