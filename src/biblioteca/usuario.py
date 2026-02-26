852a81a1a55a9aa12ccdbb289c1c4988227ad805
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-02-26 11:13:58 -0300

    Update usuario.py

--- src/biblioteca/usuario.py
+++ src/biblioteca/usuario.py
@@ -20,6 +20,6 @@ class Usuario(Pessoa):
     def get_pendencias(self):
         return self.__pendencias
     def __repr__(self):
-        return f"Nome: {self.nome} | Id: {self.get_id()} | Endereço: {self.get_endereco()}"
+        return f"Id: {self.get_id()} | Nome: {self.nome} | Endereço: {self.get_endereco()}"
     def __str__(self):
-        return f"Nome: {self.nome} | Id: {self.get_id()} | Endereço: {self.get_endereco()}"
+        return f"Id: {self.get_id()} | Nome: {self.nome} | Endereço: {self.get_endereco()}"
